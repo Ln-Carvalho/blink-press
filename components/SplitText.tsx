40c9398060b29558a5f42976749357b51816e45d
Gustavo Ferreira <gustavo.adferreira1@gmail.com>
2026-07-03 12:05:37 -0300

    Update SplitText.tsx

--- components/SplitText.tsx
+++ components/SplitText.tsx
@@ -34,7 +34,7 @@ const SplitText = ({
   text,
   className = '',
   delay = 12,
-  duration = 0.4,
+  duration = 0.03,
   ease = 'power2.out',
   splitType = 'chars',
   from = { opacity: 0, y: 10 },
