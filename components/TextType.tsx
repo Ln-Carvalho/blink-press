f5988c2cabc5c25c7d89c369fe5661b6bd01e2d6
Gustavo Ferreira <gustavo.adferreira1@gmail.com>
2026-07-03 00:05:35 -0300

    Update TextType.tsx

--- components/TextType.tsx
+++ components/TextType.tsx
@@ -23,7 +23,7 @@ const TextType = ({
   text,
   segmentTags = [],
   segmentClassNames = [],
-  typingSpeed = 12,
+  typingSpeed = 35,
   initialDelay = 0,
   pauseBetweenSegments = 120,
   showCursor = true,
